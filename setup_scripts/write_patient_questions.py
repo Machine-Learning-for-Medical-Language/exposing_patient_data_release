ede9551570596e545c3102ee3d64864b179446da
Tim Miller <tim.miller@gmail.com>
2025-05-09 14:29:47 -0400

    Updated question writing script to include dates, times, with offsets.

--- setup_scripts/write_patient_questions.py
+++ setup_scripts/write_patient_questions.py
@@ -39,14 +39,16 @@ def is_correct_format(parsed_json):
         if not "Difficulty" in keys:
             return False, "No \"Difficulty\" in a question"
 
-        if len(keys) > 3:
+        num_expected_keys = 4 if "Options" in keys else 3
+        if len(keys) > num_expected_keys:
             return False, "Unexpected field in \"Question\""
 
         return True, None
 
-MODEL = "gemma3:4b"  # or "mistral", etc.
-#URL = "http://localhost:11434/api/generate"
-URL = "http://gpu-10-0:33863/api/generate"
+#MODEL = "gemma3:4b"  # or "mistral", etc.
+MODEL = "llama3.3"
+URL = "http://localhost:11434/api/generate"
+#URL = "http://localhost:33863/api/generate"
 
 def main(args):
     if len(args) < 2:
@@ -60,7 +62,7 @@ def main(args):
     pt_year_offsets = dict()
     
     with open(args[1], "w", newline="") as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=["Patient", "Difficulty", "Question", "Answer"])
+        writer = csv.DictWriter(csvfile, fieldnames=["Patient", "Row_ID", "Level", "Difficulty", "Question", "Answer", "Options"])
         writer.writeheader()
         count = 0
         for sid in tqdm.tqdm(df['SUBJECT_ID'].unique()):
@@ -69,6 +71,7 @@ def main(args):
             first_name = subj_notes_df['FIRST_NAME'].unique()[0]
             last_name = subj_notes_df['LAST_NAME'].unique()[0]
     
+            all_notes = []
             for index, row in subj_notes_df.iterrows():
                 if not sid in pt_year_offsets:
                     year_offset = row["CHARTTIME"].year - 2018
@@ -81,12 +84,14 @@ def main(args):
                     note_date = "unknown date"
                 note_text = row["TEXT"]
                 note_type = row["CATEGORY"]
+                note_id = row["ROW_ID"]
+                all_notes.append("<note>" + note_text + "</note>")
                 try:
                     note_time = row["CHART_TIMESTAMP"].strftime('%I:%M %p')
                 except:
                     note_time = "unknown time"
                 
-                prompt = f"The following is a clinical note of type {note_type} for a patient named {first_name} {last_name} written on {note_date} at {note_time}. We are using these notes to generate a dataset of questions and answers that we will use to quiz medical students in understanding patient charts. Please generate up to three questions about this note, with varying levels of difficulty (Easy, Moderate, Difficult). The question should be self-contained, that is, contain information about the patient it's asking about and the date and approximate time of the information it's requesting. Your output should contain a clean JSON data structure (Do not include markdown, labels, or code fences), using the following schema: {{ \"Questions\": [\"Question\": <question text>, \"Answer\": <expected answer>, \"Difficulty\": <difficulty level>], ... }}. Here is the text of the note: <note> {note_text} </note>" 
+                prompt = f"The following is a clinical note of type {note_type} for a patient named {first_name} {last_name} written on {note_date} at {note_time}. We are using these notes to generate a dataset of questions and answers about facts in patient charts. They can be medical questions or general questions about things that are mentioned in the note. Please generate three or more questions about this note, with varying levels of difficulty (Easy, Moderate, Difficult). Easy questions should be True/False, Moderate questions should be multiple choice, and Difficult questions should be short answers (a single phrase or short sentence). The question should contain information about the patient it's asking about and the date and relative time of day (e.g., morning, afternoon) of the information it's requesting. The answer you provide can be slightly re-worded from the context to be clean and concise. Your output should contain a clean JSON data structure using the following schema: {{ \"Questions\": [\"Question\": <question text>, \"Answer\": <expected answer>, \"Difficulty\": <difficulty level>, \"Options\": [<multiple choice options if present>]], ... }}. Do not include markdown, labels, or code fences, just output plain JSON, Here is the text of the note: <note> {note_text} </note>" 
                 #headers: \"Question\", \"Anwer\", and \"Difficulty\". Here is the text of the note: <note> {note_text} </note>"
                 
                 
@@ -110,12 +115,50 @@ def main(args):
                     
                     for x in parsed_or_error["Questions"]:
                         x["Patient"] = sid
+                        x["Level"] = "Note"
+                        x["Row_ID"] = note_id
     
                     #valid_questions.extend(result["Questions"])
 
                     writer.writerows(parsed_or_error["Questions"])
-            if count > 10:
-                print("Quitting after 10 patients for debugging")
+                    csvfile.flush()
+            full_chart = "\n".join(all_notes)
+            prompt = f"The following is the set of clinical notes for an ICU admission for a patient named {first_name} {last_name}. We are using these notes to generate a dataset of questions and answers that we will use to quiz medical students in understanding patient charts. Please generate up to three questions about this chart, with varying levels of difficulty (Easy, Moderate, Difficult), that can be answered in a single phrase or short sentence. You should focus on questions that require synthesizing information across notes. The question should contain information about the patient it's asking about and can give or ask about specific dates and relative times (e.g., morning, afternoon) if it's important to specify. The answer you provide can be slightly re-worded from the context to be clean and concise. Your output should contain a clean JSON data structure using the following schema: {{ \"Questions\": [\"Question\": <question text>, \"Answer\": <expected answer>, \"Difficulty\": <difficulty level>], ... }}. Do not include markdown, labels, or code fences, just output plain JSON, Here is the text of the note: <chart> {full_chart} </chart>"
+            
+            response = requests.post(URL, json={
+                    "model": MODEL,
+                    "prompt": prompt,
+                    "stream": False  # You can also use True if you want streamed responses
+                })
+            
+            if response.ok:
+                result = response.json()["response"]
+                if result.startswith("```json"):
+                    result = result[8:-4]
+                format_ok, parsed_or_error = is_valid_json(result)
+                if not format_ok:
+                    print("Received invalid json: %s" % (parsed_or_error))
+                    continue
+                schema_ok, schema_msg = is_correct_format(parsed_or_error)
+                if not schema_ok:
+                    print("Received wrong schema: %s" % (schema_msg))
+                    continue
+                    
+                for x in parsed_or_error["Questions"]:
+                    x["Patient"] = sid
+                    x["Level"] = "Chart"
+    
+                #valid_questions.extend(result["Questions"])
+
+                try:
+                    writer.writerows(parsed_or_error["Questions"])
+                    csvfile.flush()
+                except Exception as e:
+                    print(f"Error trying to write csv row: {e}")
+                    continue
+
+            if count >= 100:
+                print("Quitting after 100 patients for debugging")
                 return
 
 
